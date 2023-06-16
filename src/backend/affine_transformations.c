fd6903c88ef998ef2d26f8aa10d3966b3687711d
Wilmer Norma <wilmerno@at-k6.msk.21-school.ru>
2023-06-16 18:58:32 +0300

    refactoring 3d_viewer.h

--- src/backend/affine_transformations.c
+++ src/backend/affine_transformations.c
@@ -2,41 +2,43 @@
 
 #include "3d_viewer.h"
 
-void translate_object(Object_t* obj, axis_t axis, double shift) {
+void translate_object(object_t* obj, axis_t axis, double shift) {
   for (size_t i = 0; i < obj->v_count; ++i) {
-    obj->v_matrix[i * 3 + axis] += shift;
+    obj->v_array[i].arr[axis] += shift;
   }
 }
 
-void rotate_ox_object(Object_t* obj, double angle) {
+void rotate_ox_object(object_t* obj, double angle) {
   for (size_t i = 0; i < obj->v_count; ++i) {
-    double y = obj->v_matrix[i * 3 + 1];
-    double z = obj->v_matrix[i * 3 + 2];
-    obj->v_matrix[i * 3 + 1] = y * cos(angle) - z * sin(angle);
-    obj->v_matrix[i * 3 + 1] = y * sin(angle) + z * cos(angle);
+    double y = obj->v_array[i].y;
+    double z = obj->v_array[i].z;
+    obj->v_array[i].y = y * cos(angle) - z * sin(angle);
+    obj->v_array[i].z = y * sin(angle) + z * cos(angle);
   }
 }
 
-void rotate_oy_object(Object_t* obj, double angle) {
+void rotate_oy_object(object_t* obj, double angle) {
   for (size_t i = 0; i < obj->v_count; ++i) {
-    double x = obj->v_matrix[i * 3];
-    double z = obj->v_matrix[i * 3 + 2];
-    obj->v_matrix[i * 3] = x * cos(angle) + z * sin(angle);
-    obj->v_matrix[i * 3 + 2] = -x * sin(angle) + z * cos(angle);
+    double x = obj->v_array[i].x;
+    double z = obj->v_array[i].z;
+    obj->v_array[i].x = x * cos(angle) + z * sin(angle);
+    obj->v_array[i].z = -x * sin(angle) + z * cos(angle);
   }
 }
 
-void rotate_oz_object(Object_t* obj, double angle) {
+void rotate_oz_object(object_t* obj, double angle) {
   for (size_t i = 0; i < obj->v_count; ++i) {
-    double x = obj->v_matrix[i * 3];
-    double y = obj->v_matrix[i * 3 + 1];
-    obj->v_matrix[i * 3] = x * cos(angle) - y * sin(angle);
-    obj->v_matrix[i * 3 + 2] = x * sin(angle) + y * cos(angle);
+    double x = obj->v_array[i].x;
+    double y = obj->v_array[i].y;
+    obj->v_array[i].x = x * cos(angle) - y * sin(angle);
+    obj->v_array[i].y = x * sin(angle) + y * cos(angle);
   }
 }
 
-void scale_object(Object_t* obj, double scale) {
-  for (size_t i = 0; i < 3 * obj->v_count; ++i) {
-    obj->v_matrix[i] *= scale;
+void scale_object(object_t* obj, double scale) {
+  for (size_t i = 0; i < obj->v_count; ++i) {
+    obj->v_array[i].x *= scale;
+    obj->v_array[i].y *= scale;
+    obj->v_array[i].z *= scale;
   }
 }
