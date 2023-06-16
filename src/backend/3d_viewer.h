6cb25960334c24aaa99068eba4509b6c7fabf0bd
Wilmer Norma <wilmerno@at-k6.msk.21-school.ru>
2023-06-16 16:20:24 +0300

    init main header

---
+++ src/backend/3d_viewer.h
@@ -0,0 +1,110 @@
+/* ==========================================================================*/
+/* Copyright 2023 © Moscow, Russian Federaion
+ *
+ * This file contains Original Code created by
+ * Michail *** aka sarellad,
+ * Igor Baidikov aka lorenttr,
+ * Ilya Kuzmichev aka wilmerno.
+ *
+ * The Original Code and all software developed in the process of
+ * participation on learning by experimental programming educational method.
+ * The whole methodology was developed and distributed by
+ * Autonomous non-profit organization «School 21» (ANO «School 21»).
+ *
+ * Redistribution and use of this file, its parts, or entire project
+ * are permitted by confirmation of its original creators.
+ */
+/* ==========================================================================*/
+
+/**
+ * @author sarellad, lorenttr & wilmerno
+ * @mainpage
+ * 3dViewer_v1.0, backend realised in C language, gui written
+ * on QT Creator
+ *
+ * Implementation of a program to visualise 3D wireframe models
+ *
+ * The program provide the ability to:
+ * Load a wireframe model from an obj file (vertices and surfaces list support
+ * only). Translate the model by a given distance in relation to the X, Y, Z
+ * axes. Rotate the model by a given angle relative to its X, Y, Z axes. Scale
+ * the model by a given value.
+ *
+ * The program allow customizing the type of projection (parallel and central)
+ * The program allow setting up the type (solid, dashed), color and thickness
+ * of the edges, display method (none, circle, square), color and size of the
+ * vertices
+ * The program allow choosing the background color
+ * Settings saved between program restarts
+ *
+ * The program allow saving the captured (rendered) images as bmp and jpeg
+ * files. The program allow recording small screencasts by a special button -
+ * the current custom affine transformation of the loaded object into
+ * gif-animation (640x480, 10fps, 5s)
+ */
+
+#pragma once
+
+#include <stdlib.h>
+
+/**
+ * Object_t
+ * @brief Parsed .obj structure
+ */
+typedef struct Object_t {
+  uint64_t v_count;                   ///> number of vertexes
+  uint64_t f_count;                   ///> number of faces
+  double x_min, x_max, y_min, y_max;  ///> limit values of the object
+  double* v_matrix;   ///> matrice of vertexes (each point has 3 coordinates)
+  uint64_t* f_array;  ///> array of faces
+} Object_t;
+
+/**
+ * axis_t
+ * @brief enum axes, with which it is convenient to iterate over an array of
+ * points (index + axis == x, y or z coordinate of the point)
+ */
+typedef enum axis_t { X_AXIS = 0, Y_AXIS, Z_AXIS } axis_t;
+
+// Affine transformations
+
+/**
+ * @brief Fucntion fo translate model by a given distance
+ *
+ * @param obj --- pointer to object structure
+ * @param axis --- x, y or z
+ * @param shift --- value of the shift
+ */
+void translate_object(Object_t* obj, axis_t axis, double shift);
+
+/**
+ * @brief Fucntion fo rotate model by a given angle relative to its X axis
+ *
+ * @param obj - pointer to object structure
+ * @param angle - value of the angle to rotate
+ */
+void rotate_ox_object(Object_t obj, double angle);
+
+/**
+ * @brief Fucntion fo rotate model by a given angle relative to its Y axis
+ *
+ * @param obj - pointer to object structure
+ * @param angle - value of the angle to rotate
+ */
+void rotate_oy_object(Object_t obj, double angle);
+
+/**
+ * @brief Fucntion fo rotate model by a given angle relative to its Z axis
+ *
+ * @param obj - pointer to object structure
+ * @param angle - value of the angle to rotate
+ */
+void rotate_oz_object(Object_t obj, double angle);
+
+/**
+ * @brief Fucntion fo scale model by a given value
+ *
+ * @param obj - pointer to object structure
+ * @param scale - value to scale
+ */
+void scale_object(Object_t obj, double scale);
